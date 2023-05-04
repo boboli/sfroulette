72c622720f103fcdd40c26dffbf46d2e62cfc366
Bobo Li <azncastigar@gmail.com>
2023-05-03 22:49:47 -0700

    [feature] Added error handing to webserver, cleaned up socket usage

--- sfroulette/webserver.py
+++ sfroulette/webserver.py
@@ -12,20 +12,30 @@ from http import HTTPStatus
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(HTTPStatus.OK)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        neighborhoods = get_neighborhoods_from_url(DEFAULT_NEIGHBORHOOD_URL)
-        answer = random.choice(neighborhoods)
-        msg = f"<h1>SF Roulette says to go here:</h1><h1>{answer}</h1>"
-        self.wfile.write(msg.encode())
+        try:
+            neighborhoods = get_neighborhoods_from_url(
+                DEFAULT_NEIGHBORHOOD_URL)
+            answer = random.choice(neighborhoods)
+        except Exception as error:
+            self.send_response(HTTPStatus.INTERNAL_SERVER_ERROR)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            msg = f"<h1>Something broke :(</h1><p>{error}</p>"
+            self.wfile.write(msg.encode())
+        else:
+            self.send_response(HTTPStatus.OK)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            msg = f"<h1>SF Roulette says to go here:</h1><h1>{answer}</h1>"
+            self.wfile.write(msg.encode())
 
 
-port = int(os.getenv("PORT", 80))
-print("Listening on port %s" % (port))
-httpd = socketserver.TCPServer(("", port), Handler)
-try:
-    httpd.serve_forever()
-except KeyboardInterrupt as e:
-    print("Done listening")
-    pass
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 80))
+    print(f"Listening on port {port}")
+    try:
+        with socketserver.TCPServer(("", port), Handler) as server:
+            server.serve_forever()
+    except KeyboardInterrupt:
+        print("Done listening")
+        pass
